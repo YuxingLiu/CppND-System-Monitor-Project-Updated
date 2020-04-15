0fe3ba84b6f02dc508d3f4d84d9457f226eae0fd
Yuxing <yuxingliu0822@gmail.com>
2020-04-15 18:39:28 -0400

    update Processor::Utilization()

--- include/processor.h
+++ include/processor.h
@@ -3,7 +3,11 @@
 
 class Processor {
  public:
+  Processor();
   float Utilization();
+ private:
+  long idle_ = 0;
+  long actv_ = 0;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -2,18 +2,21 @@
 #include "linux_parser.h"
 #include <unistd.h>
 
+Processor::Processor() {
+    idle_ = LinuxParser::IdleJiffies();
+    actv_ = LinuxParser::ActiveJiffies();
+}
+
 // Return the aggregate CPU utilization for 1 sec time interval
 float Processor::Utilization() {
-    long previdle = LinuxParser::IdleJiffies();
-    long prevactv = LinuxParser::ActiveJiffies();
-
-    sleep(1);
-
     long idle = LinuxParser::IdleJiffies();
     long actv = LinuxParser::ActiveJiffies();
+    long actvd = actv - actv_;
+    long totald = actvd + idle - idle_;
 
-    long actvd = actv - prevactv;
-    long totald = actvd + idle - previdle;
+    // Update the states
+    idle_ = idle;
+    actv_ = actv;
 
     return (float) actvd / (float) totald;
 }
