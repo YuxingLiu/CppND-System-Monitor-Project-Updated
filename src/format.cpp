c32f40e093e1dfcf8bb99d29328b12ee5a524b61
Yuxing <yuxingliu0822@gmail.com>
2020-05-03 17:43:09 -0400

    use sprintf to define the time string all at once

--- src/format.cpp
+++ src/format.cpp
@@ -10,11 +10,7 @@ string Format::ElapsedTime(long seconds) {
     int h = seconds/3600;
     int m = seconds%3600/60;
     int s = seconds%3600%60;
-    std::string time;
-    time += (h<10)? '0' + std::to_string(h) : std::to_string(h);
-    time += ':';
-    time += (m<10)? '0' + std::to_string(m) : std::to_string(m);
-    time += ':';
-    time += (s<10)? '0' + std::to_string(s) : std::to_string(s);
-    return time;
+    char time[10];
+    sprintf(time, "%.2i:%.2i:%.2i", h, m, s);
+    return std::string(time);
 }
